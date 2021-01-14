98e4331e6ac82d820a24684b35354da0515fbe82
Isaias Neto <isaiasneto@gmail.com>
2021-01-14 14:58:00 -0300

    Add json response

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def response():
-    return 'Hello, from flask app!'
+	return jsonify(message='Hello, from flask app!')
